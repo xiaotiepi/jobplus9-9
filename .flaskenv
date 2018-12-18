aa10efea4dc8a6a40c690ac63390f420186e2cac
xiaotiepi <45758970+xiaotiepi@users.noreply.github.com>
2018-12-18 15:00:28 +0800

    add ignore rule

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,3 @@
-FLASK_APP=bluelog
-FLASK_ENV=development
+FLASK_APP = "manage.py"
+FLASK_ENV = "development"
+FLASK_RUN_PORT = '1218'
--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 import os
-from jobplus.jobplus import create_app
+from jobplus import create_app
 from dotenv import load_dotenv
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
@@ -8,4 +8,4 @@ if os.path.exists(dotenv_path):
 app = create_app('development')
 
 if __name__ == '__main__':
-    app.run(port=1218)
+    app.run()
