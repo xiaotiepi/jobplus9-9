f337989f7f42ace378863459a938adb512a7dc5a
xiaotiepi <45758970+xiaotiepi@users.noreply.github.com>
2018-12-20 06:32:34 +0800

    fix 2 bugs in job.py and base.html

--- jobplus/handlers/job.py
+++ jobplus/handlers/job.py
@@ -9,7 +9,7 @@ def job_list():
     page = request.args.get("page", default=1, type=int)
     pagination = Job.query.order_by(Job.create_at.desc()).paginate(
         page=page,
-        per_page=current_app.confg['JOB_PER_PAGE'],
+        per_page=current_app.config['JOB_PER_PAGE'],
         error_out=False
     )
     posts = pagination.items  # 当前页数的记录列表
